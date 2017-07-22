cc9f99bf07cb83e602ff66978fff3219430563f2
hesiyi <490052981@qq.com>
2017-07-22 20:17:38 +0800

    工厂模式

---
+++ src/First/Factory/Shape.java
@@ -0,0 +1,5 @@
+package First.Factory;
+//工厂模式
+public interface Shape {
+	void draw();
+}
---
+++ src/First/Factory/Square.java
@@ -0,0 +1,11 @@
+package First.Factory;
+
+public class Square implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Square::draw() method.");
+	}
+
+}
